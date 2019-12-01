b329b79bf3e2de3c6d8d098ce135b0dbdacfd5d7
onatcipli <onatcipli@outlook.com>
2019-12-01 23:35:29 +0300

    image constants added

---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+String profileImageUrl =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGf1xTQLvI3AJz_moWk5Uj8baR9B0ffwSGyuxn9jdgiEf-fEpC';
+
+String cardBackgroundImageUrl =
+    'https://blog.macsales.com/wp-content/uploads/2019/09/macOS_Catalina_Dark.jpg';
+
