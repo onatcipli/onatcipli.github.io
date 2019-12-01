1013d1b3d1b22fb1615a9fc37660c14a68b98d7e
onatcipli <onatcipli@outlook.com>
2019-12-01 23:36:08 +0300

    DesktopView updated

--- lib/pages/desktop_view.dart
+++ lib/pages/desktop_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:onatcipli_github_io/constants/constants.dart';
+import 'package:onatcipli_github_io/constants/dark_theme.dart';
+import 'package:onatcipli_github_io/widgets/paint_triangel.dart';
 
 class DesktopView extends StatefulWidget {
   @override
@@ -8,6 +11,54 @@ class DesktopView extends StatefulWidget {
 class _DesktopViewState extends State<DesktopView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Card(
+              elevation: 5,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: CustomPaint(
+                      painter: PaintTriangel(backgroundColor: backgroundColor),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(cardBackgroundImageUrl),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Text('adasdas'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: const EdgeInsets.all(80.0),
+            child: Card(
+              child: Text('first text'),
+              elevation: 5,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
